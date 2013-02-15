2896a94350c154c604a68b9d54bebf8bad9e78f3
Meshulam Silk <moomoohk@ymail.com>
2013-02-15 02:22:17 +0200

    Added /me and /quit to the IRC

--- src/net/minelord/util/IRCBot.java
+++ src/net/minelord/util/IRCBot.java
@@ -7,17 +7,22 @@ import jerklib.events.IRCEvent;
 import jerklib.events.IRCEvent.Type;
 import jerklib.events.JoinCompleteEvent;
 import jerklib.listeners.IRCEventListener;
-import net.minelord.log.Logger;
 
 public class IRCBot implements IRCEventListener
 {
 	private Channel channel;
 	private String network, room, nick;
+	private ConnectionManager conman;
+	private IRCMessageListener messageListener;
 
 	public IRCBot(String network, String room, String nick)
 	{
-		ConnectionManager conman = new ConnectionManager(new Profile(nick));
-		conman.requestConnection(network).addIRCEventListener(this);
+		this(network, room, nick, null);
+	}
+	public IRCBot(String network, String room, String nick, IRCMessageListener messageListener)
+	{
+		this.conman = new ConnectionManager(new Profile(nick));
+		this.conman.requestConnection(network).addIRCEventListener(this);
 		this.channel = null;
 		this.nick = nick;
 		this.network = network;
@@ -32,11 +37,13 @@ public class IRCBot implements IRCEventListener
 		}
 		else
 		{
-			if (e.getType() == Type.NOTICE || e.getType() == Type.SERVER_INFORMATION)
+			if (e.getType() == Type.NOTICE || e.getType() == Type.SERVER_INFORMATION || (e.getType() == Type.DEFAULT && !e.getRawEventData().contains("KICK")) || e.getType() == Type.SERVER_VERSION_EVENT || e.getType() == Type.MOTD || e.getType() == Type.NICK_LIST_EVENT || e.getType() == Type.TOPIC)
 			{
-				Logger.logInfo(e.getRawEventData());
+				// Logger.logInfo(e.getRawEventData());
 				return;
 			}
+			if(e.getType()==Type.MODE_EVENT&&e.getRawEventData().contains("+nt"))
+				return;
 			if (e.getType() == Type.JOIN_COMPLETE)
 			{
 				JoinCompleteEvent jce = (JoinCompleteEvent) e;
@@ -64,41 +71,61 @@ public class IRCBot implements IRCEventListener
 					message = sender + " joined the room";
 				if (e.getType() == Type.QUIT)
 					message = sender + " quit";
+				if(e.getType()==Type.PART)
+					message=sender+" parted";
 				if (e.getType() == Type.DEFAULT && raw.contains("KICK"))
 				{
-					String kick = raw.substring(raw.indexOf("KICK"));
-					message = sender + " was kicked by " + kick.substring(6 + this.room.length(), kick.indexOf(":") - 1);
-					// :moomoohk!moomoohk@lu-kema2a.minelord.com KICK #Minelord
-					// moomoohk :
-					// :moomoohk!moomoohk@lu-kema2a.minelord.com KICK #Minelord
-					// moomoohk :test
+					String kick = raw.substring(raw.indexOf("KICK")), reason = kick.substring(kick.indexOf(":") + 1);
+					message = kick.substring(6 + this.room.length(), kick.indexOf(":") - 1) + " was kicked by " + sender;
+					if (reason.length() > 0)
+						message += " (" + reason + ")";
 				}
 				if (e.getType().toString().equals("MODE_EVENT"))
 				{
-					String mode = raw.substring(raw.indexOf(this.channel.getName()) + this.channel.getName().length() + 1, raw.indexOf(this.channel.getName()) + this.channel.getName().length() + 3);
-					String modeChanged = raw.substring(raw.indexOf(this.channel.getName()) + this.channel.getName().length() + 4);
-					message = "gotta catch " + mode;
-					if (mode.equals("+b"))
-						message = sender + " banned " + modeChanged;
-					if (mode.equals("-b"))
-						message = sender + " unbanned " + modeChanged;
-					if (mode.equals("+ho"))
-						message = sender + " promoted " + modeChanged + " to half operator";
-					if (mode.equals("-ho"))
-						message = sender + " demoted " + modeChanged + " from half operator";
-					if (mode.equals("+o"))
-						message = sender + " promoted " + modeChanged + " to operator";
-					if (mode.equals("-o"))
-						message = sender + " demoted " + modeChanged + " from operator";
+					try
+					{
+						String mode = raw.substring(raw.indexOf(this.channel.getName()) + this.channel.getName().length() + 1, raw.indexOf(this.channel.getName()) + this.channel.getName().length() + 3);
+						String modeChanged = raw.substring(raw.indexOf(this.channel.getName()) + this.channel.getName().length() + 4);
+						if(modeChanged.equals(this.nick))
+							modeChanged="you";
+						message = "gotta catch " + mode;
+						if (mode.equals("+b"))
+							message = sender + " banned " + modeChanged;
+						if (mode.equals("-b"))
+							message = sender + " unbanned " + modeChanged;
+						if (mode.equals("+ho"))
+							message = sender + " promoted " + modeChanged + " to half operator";
+						if (mode.equals("-ho"))
+							message = sender + " demoted " + modeChanged + " from half operator";
+						if (mode.equals("+o"))
+							message = sender + " promoted " + modeChanged + " to operator";
+						if (mode.equals("-o"))
+							message = sender + " demoted " + modeChanged + " from operator";
+						if(mode.equals("+v"))
+							message=modeChanged.equals("you")?"You were granted voice by "+sender:modeChanged+" was granted voice by "+sender;
+						if(mode.equals("-v"))
+							message=modeChanged.equals("you")?"You had your voice removed by "+sender:modeChanged+" had their voice removed by "+sender;
+					}
+					catch(Exception ex)
+					{
+						return;
+					}
 				}
 				if (message.contains("€¦"))
 					message = message.replace("€¦", "...");
 				// System.err.println(e.getType() + " : " + raw);
-				if (containsNick(message))
+				/*if (containsNick(message))
 					System.err.println(message);
 				else
 					System.out.println(message);
 				// part*/
+				if(messageListener!=null)
+					messageListener.recieveMessage(message);
+				else
+					if (containsNick(message))
+						System.err.println(message);
+					else
+						System.out.println(message);
 			}
 			catch (Exception ex)
 			{
@@ -110,10 +137,32 @@ public class IRCBot implements IRCEventListener
 
 	public void send(String message)
 	{
-		if (this.channel != null)
-			this.channel.say(message);
+		if (message.charAt(0) == '/')
+		{
+			if(message.contains(" "))
+				for(int i=0; i<message.length(); i++)
+				{
+					if(message.charAt(i)==' ')
+					{
+						System.out.println(message.length());
+						if (message.toLowerCase().contains("/me"))
+							this.channel.action(message.substring(i));
+						if(message.toLowerCase().contains("/quit"))
+							this.conman.quit(message.substring(i));
+						break;
+					}
+				}
+			else
+			{
+				if(message.toLowerCase().contains("/quit"))
+					this.conman.quit();
+			}
+		}
 		else
-			System.out.println("No channel!");
+			if (this.channel != null)
+				this.channel.say(message);
+			else
+				System.out.println("No channel!");
 	}
 
 	public boolean containsNick(String msg)
